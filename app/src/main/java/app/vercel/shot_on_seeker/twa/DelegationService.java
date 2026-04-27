418763e96c623d7cea233e82b2f0526e2673cd7f
Tee <apple@apples-MacBook-Pro-2.local>
2026-04-27 19:13:43 +0100

    add asset links

---
+++ app/src/main/java/app/vercel/shot_on_seeker/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.shot_on_seeker.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
